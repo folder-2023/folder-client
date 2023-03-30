9b5bf2f9f59bdc0bac3d091b54efde0c2da516bb
alacori <alacori1010@naver.com>
2023-03-30 16:54:19 +0900

    보호자 메인페이지

--- folder/lib/screen/protector_homescreen.dart
+++ folder/lib/screen/protector_homescreen.dart
@@ -72,204 +72,245 @@ class _ProtectorHomeScreenState extends State<ProtectorHomeScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                //처음 초록색
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              Container(
+                width: 372,
+                clipBehavior: Clip.hardEdge,
                 decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 63, 121, 114),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 30, vertical: 15),
-                          child: Text(
-                            '${selectedDate.year}년 ${selectedDate.month}월 ${selectedDate.day}일',
-                            style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 28,
-                                fontWeight: FontWeight.w600),
-                          ),
-                        )
-                      ],
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3), // changes position of shadow
                     ),
                   ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    '어르신 일정추가하기',
-                    style: TextStyle(
-                        color: Colors.teal.shade700,
-                        fontSize: 25,
-                        fontWeight: FontWeight.w700),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-              child: CalendarWidget(
-                selectedDate: selectedDate,
-                onDaySelected: onDaySelected,
-              ),
-            ),
-            FloatingActionButton(
-              onPressed: (() {
-                initState() {
-                  _eventController;
-                  _timeController;
-                }
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            '어르신 일정 추가하기',
+                            style: TextStyle(
+                              color: Colors.teal.shade700,
+                              fontSize: 28,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      CalendarWidget(
+                        selectedDate: selectedDate,
+                        onDaySelected: onDaySelected,
+                      ),
+                      FloatingActionButton(
+                        onPressed: (() {
+                          initState() {
+                            _eventController;
+                            _timeController;
+                          }
 
-                showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text('일정 추가하기'),
-                    content: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        TextField(
-                          controller: _eventController,
-                          decoration: const InputDecoration(hintText: '일정 내용'),
-                        ),
-                        TextField(
-                          controller: _timeController,
-                          decoration: const InputDecoration(
-                              hintText: '일정 시간( - - : - -)'),
-                        )
-                      ],
-                    ),
-                    actions: [
-                      TextButton(
-                          onPressed: () => Navigator.of(context).pop(),
-                          child: const Text('취소')),
-                      ElevatedButton(
-                          onPressed: () async {
-                            print(selectedDate.toIso8601String().split('T')[0]);
-                            print(_eventController.text);
-                            print(_timeController.text);
-                            final eventName = _eventController.text;
-                            final eventTime = _timeController.text;
-                            if (eventName.isNotEmpty && eventTime.isNotEmpty) {
-                              final url = Uri.parse(
-                                  'http://34.168.149.159:8080/calendar');
-                              final headers = {
-                                'Authorization': '$_accessToken',
-                                'Content-Type': 'application/json'
-                              };
-                              final response = await http.post(url,
-                                  headers: headers,
-                                  body: jsonEncode(<String, dynamic>{
-                                    'calendarDate': selectedDate
-                                        .toIso8601String()
-                                        .split('T')[0],
-                                    'calendarTime': eventTime,
-                                    'content': eventName,
-                                    //'calendarCheck': false,
-                                    //'calendarId': 0,
-                                  }));
-                              if (response.statusCode == 200) {
-                                print(response.body);
-                              } else {
-                                print(response.body);
-                              }
-                            }
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('추가하기'))
+                          showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                              title: const Text('일정 추가하기'),
+                              content: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  TextField(
+                                    controller: _eventController,
+                                    decoration: const InputDecoration(
+                                        hintText: '일정 내용'),
+                                  ),
+                                  TextField(
+                                    controller: _timeController,
+                                    decoration: const InputDecoration(
+                                        hintText: '일정 시간( - - : - -)'),
+                                  )
+                                ],
+                              ),
+                              actions: [
+                                TextButton(
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(),
+                                    child: const Text('취소')),
+                                ElevatedButton(
+                                    onPressed: () async {
+                                      print(selectedDate
+                                          .toIso8601String()
+                                          .split('T')[0]);
+                                      print(_eventController.text);
+                                      print(_timeController.text);
+                                      final eventName = _eventController.text;
+                                      final eventTime = _timeController.text;
+                                      if (eventName.isNotEmpty &&
+                                          eventTime.isNotEmpty) {
+                                        final url = Uri.parse(
+                                            'http://34.168.149.159:8080/calendar');
+                                        final headers = {
+                                          'Authorization': '$_accessToken',
+                                          'Content-Type': 'application/json'
+                                        };
+                                        final response = await http.post(url,
+                                            headers: headers,
+                                            body: jsonEncode(<String, dynamic>{
+                                              'calendarDate': selectedDate
+                                                  .toIso8601String()
+                                                  .split('T')[0],
+                                              'calendarTime': eventTime,
+                                              'content': eventName,
+                                              //'calendarCheck': false,
+                                              //'calendarId': 0,
+                                            }));
+                                        if (response.statusCode == 200) {
+                                          print(response.body);
+                                        } else {
+                                          print(response.body);
+                                        }
+                                      }
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text('추가하기'))
+                              ],
+                            ),
+                          );
+                        }),
+                        child: const Icon(Icons.add),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
                     ],
                   ),
-                );
-              }),
-              child: const Icon(Icons.add),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    '오늘의 일정',
-                    style: TextStyle(
-                        color: Colors.teal.shade700,
-                        fontSize: 25,
-                        fontWeight: FontWeight.w700),
-                  )
-                ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: _events.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final event = _events[index];
-                    if (event.date == formatDate) {
-                      return CheckboxListTile(
-                        value: iseventChecked,
-                        onChanged: ((value) {
-                          setState(() {
-                            iseventChecked = value!;
-                          });
-                        }),
-                        title: Text(event.name),
-                        subtitle: Text(event.time),
-                      );
-                    } else {
-                      return const Text('');
-                    }
-                  }),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    '약 복용',
-                    style: TextStyle(
-                        color: Colors.teal.shade700,
-                        fontSize: 25,
-                        fontWeight: FontWeight.w700),
-                  )
-                ],
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: _fills.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final fill = _fills[index];
-                    return CheckboxListTile(
-                      value: isfillChecked,
-                      onChanged: (value) {
-                        setState(() {
-                          isfillChecked = value!;
-                        });
-                      },
-                      title: Text(fill.fillName),
-                      subtitle: Text(fill.fillTime),
-                    );
-                  }),
-            ),
-          ],
+              Container(
+                width: 372,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            '오늘의 일정',
+                            style: TextStyle(
+                              color: Colors.teal.shade700,
+                              fontSize: 28,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: _events.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final event = _events[index];
+                            if (event.date == formatDate) {
+                              return ListTile(
+                                title: Text(event.name),
+                                subtitle: Text(event.time),
+                              );
+                            } else {
+                              return const Text('');
+                            }
+                          }),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 372,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            '약 복용',
+                            style: TextStyle(
+                              color: Colors.teal.shade700,
+                              fontSize: 28,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: _fills.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final fill = _fills[index];
+                            return ListTile(
+                              title: Text(fill.fillName),
+                              subtitle: Text(fill.fillTime),
+                            );
+                          }),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
         ),
       ),
     );
