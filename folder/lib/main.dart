97e8211efd56d98666cae3338a3d754ed702bde7
alacori <alacori1010@naver.com>
2023-03-30 16:53:34 +0900

    main

--- folder/lib/main.dart
+++ folder/lib/main.dart
@@ -1,34 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:folder/screen/home.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 // ignore: prefer_const_constructors
-AndroidNotificationChannel channel = AndroidNotificationChannel(
-  'high_importance_channel', // id
-  'High Importance Notifications', // title
 
-  importance: Importance.high,
-);
-
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
-  print('Handling a background message ${message.messageId}');
-}
-
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp();
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  await FlutterLocalNotificationsPlugin()
-      .resolvePlatformSpecificImplementation<
-          AndroidFlutterLocalNotificationsPlugin>()
-      ?.createNotificationChannel(channel);
-  await Firebase.initializeApp();
-
   await initializeDateFormatting();
 
   runApp(const App());
